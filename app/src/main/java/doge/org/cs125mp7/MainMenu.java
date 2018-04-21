73a64a16a37973d34677f13e414915af72086dbf
Yeshic <yeshic9@gmail.com>
2018-04-20 19:42:30 -0500

    Added the home screen with placeholder elements as well as a second activity with a place holder gif.

--- app/src/main/java/doge/org/cs125mp7/MainMenu.java
+++ app/src/main/java/doge/org/cs125mp7/MainMenu.java
@@ -1,7 +1,12 @@
 package doge.org.cs125mp7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Dog.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
