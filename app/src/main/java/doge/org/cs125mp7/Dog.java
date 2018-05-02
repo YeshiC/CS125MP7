e500a575de77eb9d829e113ca49befbc338e1025
Yeshic <yeshic9@gmail.com>
2018-05-01 21:27:08 -0500

    Finished implementing the happiness meter as ewll as the pet and feed option. Added in the ability for the dog to sleep if it is fed and happy.

--- app/src/main/java/doge/org/cs125mp7/Dog.java
+++ app/src/main/java/doge/org/cs125mp7/Dog.java
@@ -1,32 +1,177 @@
 package doge.org.cs125mp7;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import pl.droidsonroids.gif.GifTextView;
 
 public class Dog extends AppCompatActivity {
 
     static boolean exists = false;
+    //doge's level of happiness
     static int happy;
-    GifTextView dogElement;
+    //determine which interaction is selected atm
+    static int selected;
+    //gif element for dog.gif
+    static GifTextView dog;
+    //gif element for dogsleeping.gif
+    static GifTextView dogSleeping;
+    //button to select food
+    static ImageButton food;
+    //Happiness bar
+    static ImageView hBar;
+    static int tempature;
+    static ImageButton pets;
+    static boolean asleep;
+    static int hunger;
+
+    //dog.setVisibility(View.VISIBLE);
+    //food.setImageResource(R.drawable.heart2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog);
-        dogElement = findViewById(R.id.dog100);
+
+        MainMenu temp3 = new MainMenu();
+        tempature = temp3.getTemp();
+        //connects the varibles in this java class with xml elements
+        food = findViewById(R.id.food);
+        dog = findViewById(R.id.dog100);
+        dogSleeping = findViewById(R.id.dog200);
+        hBar = findViewById(R.id.red);
+        pets = findViewById(R.id.pet);
+
         if (!exists) {
             createDog();
         }
+        setHBar();
+
+        setFood();
+        main(new String[0]);
+
     }
 
+    public static void main(String[] args) {
+
+        Runnable helloRunnable = new Runnable() {
+            public void run() {
+                setHBar();
+                setFood();
+                if (hunger > 60 && asleep) {
+                    wakeUp();
+                }
+                if (hunger > 80) {
+                    happy -= hunger + 80;
+                }
+                if (hunger < 100) {
+                    hunger++;
+                }
+
+                happy -= 4;
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(helloRunnable, 0, 5, TimeUnit.SECONDS);
+
+
+        food.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                setHBar();
+
+                if (hunger < 0) {
+                    hunger = 0;
+                }
+
+                else if (hunger > 100) {
+                    hunger = 100;
+                }
+
+                else if (hunger >= 20) {
+                    happy += 5;
+                    hunger -= 30;
+                }
+
+                if (hunger < 20 && happy > 90 && !asleep) {
+                    goToSleep();
+                }
+
+                setHBar();
+            }
+        });
+
+        pets.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                happy += 10;
+                setHBar();
+            }
+        });
+
+    }
+
+
     public void createDog() {
-        happy = 100;
+        happy = 50;
+        hunger = 50;
+        asleep = false;
         exists = true;
-        for (int cnt = 0; cnt <1000; cnt++) {
-            dogElement.setVisibility(View.VISIBLE);
+    }
+
+    public static void goToSleep() {
+        dog.setVisibility(View.INVISIBLE);
+        dogSleeping.setVisibility(View.VISIBLE);
+    }
+
+    public static void wakeUp() {
+        dog.setVisibility(View.VISIBLE);
+        dogSleeping.setVisibility(View.INVISIBLE);
+    }
+
+    public static void setHBar() {
+
+        MainMenu temp2 = new MainMenu();
+        setFood();
+        if (happy > 95) {
+            happy = 95;
+        }
+        if (happy < 0) {
+            happy = 0;
+        }
+        ViewGroup.LayoutParams params = hBar.getLayoutParams();
+        params.height = happy * 6 + 30;
+        hBar.setLayoutParams(params);
+        tempature = temp2.getTemp();
+        setFood();
+    }
+
+    public static void setFood() {
+        if (tempature < 40) {
+            food.setImageResource(R.drawable.food3);
+        }
+        else if (tempature > 95) {
+            food.setImageResource(R.drawable.food4);
+        }
+        else {
+            food.setImageResource(R.drawable.food2);
         }
     }
 
--- app/src/main/java/doge/org/cs125mp7/MainMenu.java
+++ app/src/main/java/doge/org/cs125mp7/MainMenu.java
@@ -3,24 +3,119 @@ package doge.org.cs125mp7;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainMenu extends AppCompatActivity {
 
+    private static String json;
+
+    private static final String TAG = "MP7";
+
+    /**
+     * Request queue for our network requests.
+     */
+    private static RequestQueue requestQueue;
+
+    String provider;
+    //Location location;
+    String locationString = "";
+    //double longitude;
+//double latitude;
+    public static String cityname = "Champaign";
+    int temp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        final Button startAPICall = findViewById(R.id.button);
+        //LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            Log.d(TAG, "OPPS");
+//            return;
+//        }
+//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        longitude = location.getLongitude();
+//        latitude = location.getLatitude();
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button2 clicked");
+                startAPICall();
+
+
+            }
+        });
+
     }
 
-    public void sendMessage(View view) {
+
+    void startAPICall() {
+        try {
+
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=b0afd3047fb4468bae6220328182504&q=" + cityname + "&num_of_days=2&tp=3&format=json",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            json = response.toString();
+                            Log.d(TAG, response.toString());
+                            json = response.toString();
+                            String json = response.toString();
+                            JsonParser parser = new JsonParser();
+                            JsonObject result = parser.parse(json).getAsJsonObject();
+                            JsonObject data = result.getAsJsonObject("data");
+                            JsonArray condition = data.getAsJsonArray("current_condition");
+                            JsonObject ob = condition.get(0).getAsJsonObject();
+                            temp = ob.get("temp_F").getAsInt();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
         Intent intent = new Intent(this, Dog.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         //EditText editText = (EditText) findViewById(R.id.editText);
         //String message = editText.getText().toString();
         //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    public int getTemp(){
+        return temp;
+    }
+
 }
